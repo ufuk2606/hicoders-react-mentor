8c355e5a37c0d292294d3ccc4de2ea1f9af7f137
ufuk2606 <ufukozkar11@gmail.com>
2022-12-20 12:54:44 +0100

    week1-assignment1-added

---
+++ week1-assignment1/src/app.js
@@ -0,0 +1,9 @@
+import { renderNav, renderContent, renderFooter } from "./function.js";
+/**
+ * Importing these functions from function.js and rendering.
+ */
+(function render() {
+  renderNav();
+  renderContent();
+  renderFooter();
+}());
