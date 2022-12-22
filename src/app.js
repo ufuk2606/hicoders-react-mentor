89e5ad0b83c0c0ca7d3288931e405ca280bf855b
ufuk2606 <ufukozkar11@gmail.com>
2022-12-22 14:29:48 +0100

    week1-assignment2-added

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import {areaOfTriangle,areaOfRectangle,areaOfCircle} from "./calculateArea.js"
+
+
+console.log(areaOfTriangle(12,3))//18
+console.log(areaOfRectangle(12,3))//36
+console.log(areaOfCircle(12))//452.39
